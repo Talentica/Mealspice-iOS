87d6baa154c7641932e7589a92f6b5ad1bf87963
Nishu Priya <Nishu.Priya@talentica.com>
2018-07-27 21:15:22 +0530

    Removing testcase

--- MealSpiceTests/ViewModel/RestaurantListViewModelTests.swift
+++ MealSpiceTests/ViewModel/RestaurantListViewModelTests.swift
@@ -17,81 +17,34 @@ class RestaurantListViewModelTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        viewModel = RestaurantListViewModel(dataSource: dataSource)
     }
     override func tearDown() {
         super.tearDown()
-        viewModel = nil
     }
     
-    func testSortByName() {
-        let rest1 = Restaurant()
-        rest1.name = "BRest"
-        let rest2 = Restaurant()
-        rest2.name = "ARest"
-        let list = [rest1, rest2]
-        let desiredList = [rest2, rest1]
-        let endList = viewModel.sortListByName(list: list)
-        if endList == desiredList {
-            XCTAssert(true)
-        }
-    }
-    func testSortByRate() {
-        let rest1 = Restaurant()
-        rest1.rating = 1
-        let rest2 = Restaurant()
-        rest2.rating = 2
-        let rest3 = Restaurant()
-        rest3.rating = 3
-        let list = [rest1, rest2]
-        
-        let desiredList = [rest3, rest2, rest1]
-        let endList = viewModel.sortListByName(list: list)
-        if endList == desiredList {
-            XCTAssert(true)
-        }
-    }
-    func testFetchWithNoService() {
-        // giving no service to a view model
-        viewModel.service = nil
-        // expected to not be able to fetch currencies
-        viewModel.fetchRestaurantList() { result in
-            switch result {
-            case .success(_) :
-                XCTAssert(false, "ViewModel should not be able to fetch without service")
-            default:
-                break
-            }
-        }
-    }
-    func testGetRestaurantSuccessList() {
-        let mockWebservice = MockWebService()
-        mockWebservice.restaurants = [Restaurant()]
-        viewModel.service = mockWebservice
-        viewModel.fetchRestaurantList() {
-            result in
-            switch result {
-            case .success :
-                XCTAssert(true, "There is one restaurant, and it should be success")
-            case .failure :
-                XCTAssert(false, "There is one restaurant, and it should be success")
-            }
-            
-        }
-    }
-    func testGetRestaurantListFailure() {
-        let mockWebservice = MockWebService()
-        viewModel.service = mockWebservice
-        viewModel.fetchRestaurantList() {
-            result in
-            switch result {
-            case .success :
-                XCTAssert(true, "There is no restaurant, and it should be failure")
-            case .failure :
-                XCTAssert(true, "There are no restaurants, and it should be failure")
-            }
-        }
-    }
+//    func testSortByName() {
+//    }
+    
+    
+    
+    
+//    func testSortByRate() {
+//    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    func testFetchWithNoService() {
+//    }
+//    func testGetRestaurantSuccessList() {
+//    }
+//    func testGetRestaurantListFailure() {
+//    }
 }
 
 fileprivate class MockWebService: RestaurantServiceProtocol {
--- MealSpiceUITests/MealSpiceUITests.swift
+++ MealSpiceUITests/MealSpiceUITests.swift
@@ -9,23 +9,18 @@
 import XCTest
 
 class MealSpiceUITests: XCTestCase {
+    
     var app: XCUIApplication!
     let TIMEOUT = TimeInterval(1)
-    let KRestaurantViewTitle = "Restaurant List"
     let kRestaurantDetailTitle = "Resturant Detail"
     
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         app = XCUIApplication()
         app.launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
@@ -35,55 +30,16 @@ class MealSpiceUITests: XCTestCase {
 
     func testExample() {
         
-
     }
     
-    func testCellImageSize() {
-        let cell = app/*@START_MENU_TOKEN@*/.tables["Restaurant_List_Table"]/*[[".otherElements[\"Restaurant_ListSuperView\"].tables[\"Restaurant_List_Table\"]",".tables[\"Restaurant_List_Table\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.cells["cell_0"]
-        let exists = cell.waitForExistence(timeout: 0.3)
-        if exists {
-            let frame = cell.images["restaurantImageView"].frame
-            XCTAssert(frame.size.height == 74 && frame.size.width == 74, "Check image frame")
-        } else {
-            XCTAssert(false, "Wait failed")
-        }
-    }
-    
-    func testNavigationTitleForDetailScreenExists() {
-        let cell = app.tables["Restaurant_List_Table"].cells["cell_1"]
-        cell.waitForExistence(timeout: 2.0)
-        cell.tap()
-        let nextPageNavigationTitle = app.navigationBars["Restaurant Detail"]
-        if nextPageNavigationTitle.waitForExistence(timeout: 0.3) == true {
-            XCTAssert(true, "Element found")
-        } else {
-            XCTAssert(false, "Failed to find element")
-        }
-    }
-    
-    func testBothButtonsEnabledAtStart() {
-        let nameButton = app.buttons["name_button"]
-        let ratingButton = app.buttons["rating_button"]
-        
-        if nameButton.exists && ratingButton.exists {
-            XCTAssert((nameButton.isEnabled && ratingButton.isEnabled), "checking if both buttons are enabled")
-        } else {
-            XCTAssert(false, "Buttons not present")
-        }
-    }
-    
-    func testRatingButtonBecomesDisabledOnceTapped() {
-        let nameButton = app.buttons["name_button"]
-        let ratingButton = app.buttons["rating_button"]
-        
-        if nameButton.exists && ratingButton.exists {
-            nameButton.tap()
-            XCTAssert((!nameButton.isEnabled && ratingButton.isEnabled), "checking if both buttons are enabled")
-            
-            ratingButton.tap()
-            XCTAssert((nameButton.isEnabled && !ratingButton.isEnabled), "checking if both buttons are enabled")
-        } else {
-            XCTAssert(false, "Buttons not present")
-        }
-    }
+//    func testCellImageSize() {
+//    }
+//
+//    func testNavigationTitleForDetailScreenExists() {
+//    }
+//
+//    func testBothButtonsEnabledAtStart() {
+//    }
+//    func testRatingButtonBecomesDisabledOnceTapped() {
+//    }
 }
